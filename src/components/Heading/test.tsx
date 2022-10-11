45527c48b9d22a831b9a2b6d6780e2bde626dc11
Wildmarks Passos <wildmarks@gmail.com>
2022-10-10 21:55:42 -0300

    Adicionado alguns testes de estilos do componente Heading

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,13 +1,32 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Heading />)
+  it('should render the heading with the following default styles', () => {
+    renderWithTheme(<Heading>Won Games</Heading>)
 
-    expect(screen.getByRole('heading', { name: /Heading/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyle({
+      color: '#FAFAFA'
+    })
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyleRule(
+      'background-color',
+      '#35BDAC',
+      { modifier: '::before' }
+    )
+
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyleRule(
+      'padding-left',
+      '0.7rem',
+      { modifier: '::before' }
+    )
+
+    expect(screen.getByRole('heading', { name: /Won Games/i })).toHaveStyleRule(
+      'margin-right',
+      '1.1rem',
+      { modifier: '::before' }
+    )
   })
 })
